1a91e4351a7d500225c05b3a28f96d4c7ec16c3e
FlavioVissoto <vissoto_flavio@hotmail.com>
2023-02-13 23:37:42 -0300

    Button login

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from './button/button.component';
 import { InputComponent } from './input/input.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
-  declarations: [ButtonComponent, InputComponent],
-  exports: [ButtonComponent, InputComponent],
+  declarations: [ButtonComponent, InputComponent, CheckboxComponent],
+  exports: [ButtonComponent, InputComponent, CheckboxComponent],
   imports: [CommonModule, FormsModule],
 })
 export class ComponentsModule {}
